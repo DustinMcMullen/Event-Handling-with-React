ee0e84251d70e64dcbd7389d1df3ff4e8cf6aee7
DustinMcMullen <51675499+DustinMcMullen@users.noreply.github.com>
2020-09-02 22:47:12 -0500

    Updated to handle click & hover events

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+
+// const [headingText, setHeadingText] = useState("Hello");
 
 function App() {
+  const [headingText, setHeadingText] = useState("Hello");
+  const [buttonColor, setButtonColor] = useState({ backgroundColor: "white" });
+
+  function updateHeadingText() {
+    setHeadingText(headingText + " Dustin");
+  }
+
+  function lighten() {
+    setButtonColor({ backgroundColor: "white" });
+  }
+
+  function darken() {
+    setButtonColor({ backgroundColor: "black" });
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>{headingText}</h1>
       <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <button
+        style={buttonColor}
+        onMouseOver={darken}
+        onMouseOut={lighten}
+        onClick={updateHeadingText}
+      >
+        Submit
+      </button>
     </div>
   );
 }
